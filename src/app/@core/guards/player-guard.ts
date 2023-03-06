8ca16b1c0f0b59cd19086d95e596ddb99f3386ad
Your Name <you@example.com>
2023-03-06 23:54:24 +0530

    Daily Commit

--- src/app/@core/guards/player-guard.ts
+++ src/app/@core/guards/player-guard.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot,RouterStateSnapshot } from '@angular/router';
-import { Observable, of, tap } from 'rxjs';
+import { Router, CanActivate, ActivatedRouteSnapshot,RouterStateSnapshot, ActivatedRoute } from '@angular/router';
+import { map, Observable, of, tap } from 'rxjs';
+import { ScoreService } from '../services/score/score.service';
 import { UserService } from '../services/user/user.service';
  
  
 @Injectable()
 export class PlayerGuard implements CanActivate {
  
-    constructor(private _router:Router, private api:UserService ) {
+    constructor(private _router:Router, private api:UserService, private score: ScoreService, private route: ActivatedRoute ) {
     }
  
-    canActivate(route: ActivatedRouteSnapshot,
-                state: RouterStateSnapshot): Observable<boolean> {
+    canActivate(route: ActivatedRouteSnapshot, 
+                state: RouterStateSnapshot): Observable<boolean> {             
+                    if (state.url.indexOf('/playerDashboard') > -1) {
+                        return this.score.getMatchDetailsByMatchNo(route.url[1].path).pipe(map(o=>o.enable11),tap(o=>{
+                            if (o) {
+                                
+                            }
+                        }))
+                      }
                     return of(true)
 
     }
